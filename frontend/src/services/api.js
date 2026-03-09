53f7f1f211ab850e7afa5a0774300e1d7cc435be
tdvoday <vohgphuc@gmail.com>
2026-03-09 16:15:57 +0700

    call backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// Thay bằng IP máy tính của bạn (không dùng localhost)
+// Thay 192.168.x.x bằng IP máy tính của bạn (chạy ipconfig để xem)
 const BASE_URL = "http://192.168.0.101:5000/api";
 
 const api = axios.create({ baseURL: BASE_URL });
 
-// Tự động gắn token vào mỗi request
 api.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
